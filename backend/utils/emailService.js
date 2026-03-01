c5fda617d514b1d1d1bb3e0b519582f244676938
meetjaka <meetjaka46@gmail.com>
2026-03-01 22:44:45 +0530

    Result send to Email

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -19,7 +19,7 @@ const createTransporter = () => {
   } else {
     // Development - use Gmail or Ethereal
     // For Gmail, enable "Less secure app access" or use App Password
-    return nodemailer.createTransporter({
+    return nodemailer.createTransport({
       service: "gmail",
       auth: {
         user: process.env.EMAIL_USER || "your-email@gmail.com",
